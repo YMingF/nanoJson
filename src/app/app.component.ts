7f5286e667db33af2d1d9d776fcdccfdeabc0548
alex <alex@nanoinsure.com>
2024-08-14 16:57:25 +0800

    implement doc data process logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,115 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import JSZip from 'jszip';
+import { DocProcessService } from './services/doc-process.service';
+import 'jsoneditor/dist/jsoneditor.css';
+import { JSONEditorOptions } from 'jsoneditor';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  jsonEditor: any;
+  constructor(
+    private docProcessSvc: DocProcessService,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+  ngOnInit() {}
+  ngAfterViewInit() {
+    this.initializeJsonEditor();
+    this.subscribeFileUpload();
+  }
+
+  private subscribeFileUpload() {
+    this.document
+      .getElementById('upload-docx')!
+      .addEventListener('change', (event) => {
+        const file = (event.target as HTMLInputElement).files?.[0];
+        file && this.readDocx(file);
+      });
+  }
+
+  private initializeJsonEditor() {
+    if (typeof window !== 'undefined') {
+      import('jsoneditor').then((JSONEditorModule) => {
+        const JSONEditor = JSONEditorModule.default;
+        const container = document.getElementById('jsonEditor');
+        if (!container) {
+          return;
+        }
+        const options = { mode: 'code' } as JSONEditorOptions;
+        this.jsonEditor = new JSONEditor(container, options);
+      });
+    }
+  }
+
+  uploadFile() {
+    this.document.getElementById('upload-docx')?.click();
+  }
+
+  readDocx(file: Blob) {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const arrayBuffer = event.target?.result;
+      if (!arrayBuffer) {
+        return;
+      }
+      this.processDocx(arrayBuffer as Buffer);
+    };
+
+    reader.readAsArrayBuffer(file as Blob);
+  }
+
+  processDocx(arrayBuffer: Buffer) {
+    JSZip.loadAsync(arrayBuffer)
+      .then(function (zip) {
+        return zip.file('word/document.xml')?.async('string');
+      })
+      .then((xmlContent) => {
+        this.parseXML(xmlContent || '');
+      })
+      .catch(function (err) {
+        console.error('Error reading docx file:', err);
+      });
+  }
+
+  parseXML(xmlContent: string) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlContent, 'application/xml');
+    const tableData = [];
+
+    const tables = xmlDoc.getElementsByTagName('w:tbl');
+
+    for (let i = 0; i < tables.length; i++) {
+      const table = tables[i];
+      const rows = table.getElementsByTagName('w:tr');
+
+      for (let j = 0; j < rows.length; j++) {
+        const row = rows[j];
+        const cells = row.getElementsByTagName('w:tc');
+
+        let rowData = [];
+        for (let k = 0; k < cells.length; k++) {
+          const cell = cells[k];
+          const paragraphs = cell.getElementsByTagName('w:t');
+          let cellText = '';
+
+          for (let p = 0; p < paragraphs.length; p++) {
+            cellText += paragraphs[p].textContent;
+          }
+
+          rowData.push(cellText.trim());
+        }
+        tableData.push(rowData);
+      }
+    }
+    this.jsonEditor.set(this.docProcessSvc.processTableData(tableData));
+  }
 }
---
+++ src/app/services/doc-process.service.ts
@@ -0,0 +1,152 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DocProcessService {
+  extractText(input: string): { en: string; tc: string } {
+    const enPattern = /[A-Za-z][^\u4e00-\u9fff]*/;
+    const tcPattern = /[\u4e00-\u9fff][^A-Za-z]*/;
+
+    const enMatch = input.match(enPattern);
+    const tcMatch = input.match(tcPattern);
+
+    return {
+      en: enMatch ? enMatch[0].trim() : '',
+      tc: tcMatch ? tcMatch[0].trim() : '',
+    };
+  }
+
+  processTableData(data: string[][]): any[] {
+    const jsonData = data.map((row) => {
+      const fieldName = row[1];
+      const fieldType = row[2];
+      const remark = row[3];
+      const { en, tc } = this.extractText(fieldName.replace('\n', ' '));
+
+      const entry: any = {
+        label: { en, tc },
+        custClass: 'input-field',
+        id: this.generateId(fieldName),
+      };
+
+      // 根据 fieldType 插入不同的类型
+      if (/picker/i.test(fieldType)) {
+        Object.assign(entry, {
+          type: 'customPicker',
+          subType: 'filter',
+          canClear: true,
+          placeholder: {
+            en: 'Please select',
+            tc: '請選擇',
+          },
+        });
+      } else if (/text/i.test(fieldType)) {
+        entry.type = 'text';
+        this.setMax(fieldType, entry);
+      } else if (/num/i.test(fieldType)) {
+        entry.type = 'number';
+        entry.subType = 'desktop';
+        this.setMax(fieldType, entry);
+      } else if (/label/i.test(fieldType)) {
+        entry.type = 'label';
+        delete entry.id;
+      } else if (/Radio/i.test(fieldType)) {
+        entry.type = 'radios';
+        entry.canUnselect = true;
+      } else if (/Section header/i.test(fieldType)) {
+        entry.type = 'section';
+        entry.custClass = 'section-box-header';
+      } else if (/date/i.test(fieldType)) {
+        entry.type = 'multi_format_date';
+      }
+
+      // 根据 remark 插入只读属性
+      if (/\bread only\b/i.test(remark)) {
+        entry.readonly = true;
+      } else if (/\bMandatory\b/i.test(remark)) {
+        entry.mandatory = true;
+      }
+
+      return entry;
+    });
+    console.log(`jsonData`, jsonData);
+    return jsonData;
+  }
+
+  private generateId(text: string): string {
+    const cleanedText = text
+      .replace(/[^a-zA-Z0-9\s]/g, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()
+      .toLowerCase();
+    const words = cleanedText.split(' ');
+    const stopWords = new Set([
+      'the',
+      'and',
+      'or',
+      'a',
+      'an',
+      'in',
+      'on',
+      'with',
+      'for',
+      'to',
+      'from',
+      'by',
+      'of',
+      'is',
+      'are',
+      'was',
+      'were',
+      'this',
+      'that',
+      'these',
+      'those',
+      'it',
+      'its',
+      'as',
+      'at',
+      'be',
+      'but',
+      'not',
+    ]);
+
+    const filteredWords = words.filter((word) => !stopWords.has(word));
+    // @ts-ignore
+    const wordFreq = filteredWords.reduce(
+      (acc: any, word: string) => ((acc[word] = (acc[word] || 0) + 1), acc),
+      {}
+    );
+    // @ts-ignore
+    const sortedWords = Object.keys(wordFreq).sort(
+      (a, b) => wordFreq[b] - wordFreq[a] || b.length - a.length
+    );
+
+    return sortedWords
+      .slice(0, 3)
+      .map((word, index) =>
+        index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+      )
+      .join('');
+  }
+
+  setMax(strVal: string, entry: any): void {
+    const matchRes = /\((\d+),?(\d+)?\)/.exec(strVal);
+    if (matchRes) {
+      const integerPartLength = parseInt(matchRes[1], 10);
+      const decimalPartLength = matchRes[2] ? parseInt(matchRes[2], 10) : 0;
+
+      // 动态生成 max 值，例如 4 -> 9999, 3 -> 999
+      entry.max = parseFloat(
+        '9'.repeat(integerPartLength) +
+          (decimalPartLength > 0 ? '.' + '9'.repeat(decimalPartLength) : '')
+      );
+
+      // 设置 decimal 属性，如果存在
+      if (decimalPartLength > 0) {
+        entry.decimal = decimalPartLength;
+      }
+    }
+  }
+}
